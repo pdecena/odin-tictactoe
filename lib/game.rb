1d54889b7113a50c47119f791c571234fb9cd20a
pdecena <paolo.decena@gmail.com>
2025-01-26 22:00:50 +0900

    Update game and main

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,19 @@
 class Game
 
-	@game_on_flag = 0
-	@coord_array = [["A1","A2","A3"],["B1","B2","B3"],["C1","C2","C3"]]
-
-	def init_game
+	def initialize
+	  @game_on_flag = 0
+	  @coord_array = [["A1","A2","A3"],["B1","B2","B3"],["C1","C2","C3"]]
+	  @player_turn = 0
 	end
 
-	def play_game(coord_array,game_on_flag,player_turn)
+	def start_game
+	end
+
+	def continue_game
 		puts "Enter the position you want to play in:"
 		position_in = gets.chomp
 		puts "Received input: " + position_in
-		coord_array.each do |pos|
+		@coord_array.each do |pos|
 			if pos == position_in
 				if player_turn == 0
 					pos = X
--- main.rb
+++ main.rb
@@ -6,4 +6,4 @@ playerOne = Player.new.define_player_one
 playerTwo = Player.new.define_player_two
 
 game = Game.new
-game.play_game(game:coord_array,game:game_on_flag,1)
+game.continue_game
